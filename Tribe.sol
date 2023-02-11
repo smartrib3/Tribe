3205f8700d0ee459fd3beb83e73e4a9868d4d214
SMARTRIB3 <94409591+smartrib3@users.noreply.github.com>
2023-02-10 16:34:09 -0800

    Create Tribe.sol

---
+++ Tribe.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.0;
+
+contract Tribe {
+    struct Member {
+        uint256 id;
+        string name;
+        uint256 age;
+        string location;
+    }
+
+    mapping (uint256 => Member) public members;
+    uint256 public memberCount;
+
+    function addMember(uint256 _id, string memory _name, uint256 _age, string memory _location) public {
+        members[memberCount] = Member(_id, _name, _age, _location);
+        memberCount++;
+    }
+
+    function getMember(uint256 _id) public view returns (uint256, string memory, uint256, string memory) {
+        Member storage member = members[_id];
+        return (member.id, member.name, member.age, member.location);
+    }
+}
