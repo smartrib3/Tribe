380bac2162353d9b83d6976886f08b1ae808e12b
SMARTRIB3 <94409591+smartrib3@users.noreply.github.com>
2023-02-13 19:17:49 -0800

    Update Tribe.sol
    the "location" property of the "Member" struct has been changed to "tribe" to keep track of the tribe that the member is from.
    
    The function parameter and struct property names have also been changed from "location" to "tribe" to reflect the modification.
    
    Finally, the function return statement in the "getMember" function has been updated to return the "tribe" property instead of the old "location" property.

--- Tribe.sol
+++ Tribe.sol
@@ -5,19 +5,19 @@ contract Tribe {
         uint256 id;
         string name;
         uint256 age;
-        string location;
+        string tribe;
     }
 
     mapping (uint256 => Member) public members;
     uint256 public memberCount;
 
-    function addMember(uint256 _id, string memory _name, uint256 _age, string memory _location) public {
-        members[memberCount] = Member(_id, _name, _age, _location);
+    function addMember(uint256 _id, string memory _name, uint256 _age, string memory _tribe) public {
+        members[memberCount] = Member(_id, _name, _age, _tribe);
         memberCount++;
     }
 
     function getMember(uint256 _id) public view returns (uint256, string memory, uint256, string memory) {
         Member storage member = members[_id];
-        return (member.id, member.name, member.age, member.location);
+        return (member.id, member.name, member.age, member.tribe);
     }
 }
